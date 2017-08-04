358d20e149cd76a9710e6cb7a26cbada8d182657
zhao361533013 <1756416778@qq.com>
2017-08-04 23:37:18 +1000

    Create sushu.c
    输入一个数 求出小于这个数的素数

---
+++ sushu.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main ()
+{
+ int maxNumber;
+ printf ("Please type a number\n");
+ scanf ("%d",&maxNumber);
+ int isPrime[maxNumber];
+ int i,x;
+ 
+ for (i=0;i<maxNumber;i++)
+ {
+  isPrime[i] =1;
+ }
+for (x=2;x<maxNumber;x++)
+{
+	if(isPrime[x])
+	{
+		for(i=2; i*x<maxNumber;i++)
+		{
+			isPrime[i*x]=0;
+		}
+		
+	}
+}
+for (i=2;i<maxNumber;i++){
+	if (isPrime[i]){
+		printf("%d\t",i);
+	}
+	
+}	
+	printf("\n");
+	return 0;
+}
